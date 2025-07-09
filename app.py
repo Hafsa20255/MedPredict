d47368f16ae7303402ccf363d1b4bd6f147a4b26
Hafsa20255 <hafsahsain02@gmail.com>
2025-07-09 23:06:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
-# Titre et description
-st.markup("""
+# Bannière en haut
+st.markdown("""
 <div style='background-color:#1A237E; padding:15px; border-radius:10px;'>
     <h1 style='color:white; text-align:center;'>MedPredict</h1>
     <p style='color:white; text-align:center;'>AI-powered predictive maintenance for medical equipment</p>
 </div>
 """, unsafe_allow_html=True)
+
+# Titre et description
 st.title("AI-powered Predictive Maintenance")
 
 st.markdown(
